b5b485284ada5bbd28b4d7005324f9b102c29f25
Murph <max_obey@hotmail.com>
2020-06-29 01:22:42 +0700

    Add file instagramdownload.py

---
+++ instagramdownload.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+def GetUrlImage(URL):
+	r = requests.get(URL)
+	htmlParser = BeautifulSoup(r.text, 'html.parser')
+	# linkImage = soup.find_all("meta",{"property":"og:image"})
+	metaFile = htmlParser.find(property="og:image")
+	actualURLImage = metaFile['content']
+	return actualURLImage
+
+def GetNameImage(URL):
+	URLWasSplitBySlash = URL.split('/')[-1]
+	imageFileName = URLWasSplitBySlash.split('?')[0]
+	return imageFileName
+
+def DowloadImage(nameImage, URLImage):
+	r = request.get(URLImage)
+	downloadCommand = '''wget -o {} "{}"'''
+	os.system(downloadCommand.format(nameImage, URLImage))
+
+
+
+# from selenium import webdriver
+# driver = webdriver.Firefox(executable_path=r'C:\Users\*****\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe')
+# driver.get(link)
+# html = driver.execute_script("return document.documentElement.outerHTML;")
+# html = driver.execute_script("return document.documentElement.innerHTML;")
+# html = driver.page_source
+# find_all("img",{"style":"object-fit: cover;"})
