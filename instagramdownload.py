ccf84dc92f0c7ff3c66e9701f736d02853a47584
lastorder999 <max_obey@hotmail.com>
2020-07-08 04:45:43 +0700

    change html to json for get image url

--- instagramdownload.py
+++ instagramdownload.py
@@ -1,30 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import json
 import sys
 
 
+def URLSingleImage(jsonFile):
+	return jsonFile['graphql']['shortcode_media']['display_url'] 
+
+def URLMultipleMedia(jsonFile):
+	findURL = jsonFile['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
+	return [URLs['node']['display_url'] for URLs in findURL]
+	
+def URLSingleVideo(jsonFile):
+	return jsonFile['graphql']['shortcode_media']['video_url']
+
 def GetUrlImage(URL):
-	r = requests.get(URL)
-	htmlParser = BeautifulSoup(r.text, 'lxml')
-	metaFile = htmlParser.find(property="og:image")
-	actualURLImage = metaFile['content']
-	return actualURLImage
+	add = '?__a=1'
+	link = URL + add
+	r = requests.get(link)
+	jsonFile = r.json()
+	typeFile = jsonFile['graphql']['shortcode_media']['__typename']
+	if typeFile == "GraphImage":
+		return URLSingleImage(jsonFile)
+	elif typeFile == "GraphVideo":
+		return URLSingleVideo(jsonFile)
+	elif typeFile == "GraphSidecar":
+		return URLMultipleMedia(jsonFile)
 
 def GetNameImage(URLImage):
-	URLWasSplitBySlash = URLImage.split('/')[-1]
-	imageFileName = URLWasSplitBySlash.split('?')[0]
-	return imageFileName
+	URLSplit = URLImage.split('/')[-1]
+	URLSplit = URLSplit.split('?')[0]
+	return URLSplit
 
 def DowloadImage(nameImage, URLImage):
 	r = requests.get(URLImage)
+
 	with open(nameImage, 'wb') as fileImage:
 		fileImage.write(r.content)
 	
 def main(url):
 	urlimage = GetUrlImage(url)
-	nameimage = GetNameImage(urlimage)
-	DowloadImage(nameimage, urlimage)
+	
+	if type(urlimage) is list:
+		for urls in urlimage:
+			DowloadImage(GetNameImage(urls), urls)
+	else:
+		DowloadImage(GetNameImage(urlimage),urlimage)
 
 if __name__ == '__main__':
 	main(sys.argv[1])
