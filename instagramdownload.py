722aa700026e7dd830d233839eb752e05acbfaf7
lastorder999 <max_obey@hotmail.com>
2020-08-10 03:19:16 +0700

    formatting style pep8

--- instagramdownload.py
+++ instagramdownload.py
@@ -1,51 +1,58 @@
+#!/sr/bin python3
 import requests
-import lxml
-import json
 import sys
 
 
 def URLSingleImage(jsonFile):
-	return jsonFile['graphql']['shortcode_media']['display_url'] 
+    return jsonFile['graphql']['shortcode_media']['display_url']
+
 
 def URLMultipleMedia(jsonFile):
-	findURL = jsonFile['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
-	return [URLs['node']['display_url'] for URLs in findURL]
-	
+    findURL = jsonFile['graphql']['shortcode_media'][
+        'edge_sidecar_to_children']['edges']
+    return [URLs['node']['display_url'] for URLs in findURL]
+
+
 def URLSingleVideo(jsonFile):
-	return jsonFile['graphql']['shortcode_media']['video_url']
+    return jsonFile['graphql']['shortcode_media']['video_url']
+
 
 def GetUrlImage(URL):
-	add = '?__a=1'
-	link = URL + add
-	r = requests.get(link)
-	jsonFile = r.json()
-	typeFile = jsonFile['graphql']['shortcode_media']['__typename']
-	if typeFile == "GraphImage":
-		return URLSingleImage(jsonFile)
-	elif typeFile == "GraphVideo":
-		return URLSingleVideo(jsonFile)
-	elif typeFile == "GraphSidecar":
-		return URLMultipleMedia(jsonFile)
+    add = '?__a=1'
+    link = URL + add
+    r = requests.get(link)
+    jsonFile = r.json()
+    typeFile = jsonFile['graphql']['shortcode_media']['__typename']
+    if typeFile == "GraphImage":
+        return URLSingleImage(jsonFile)
+    elif typeFile == "GraphVideo":
+        return URLSingleVideo(jsonFile)
+    elif typeFile == "GraphSidecar":
+        return URLMultipleMedia(jsonFile)
+
 
 def GetNameImage(URLImage):
-	URLSplit = URLImage.split('/')[-1]
-	URLSplit = URLSplit.split('?')[0]
-	return URLSplit
+    URLSplit = URLImage.split('/')[-1]
+    URLSplit = URLSplit.split('?')[0]
+    return URLSplit
+
 
 def DowloadImage(nameImage, URLImage):
-	r = requests.get(URLImage)
+    r = requests.get(URLImage)
 
-	with open(nameImage, 'wb') as fileImage:
-		fileImage.write(r.content)
-	
+    with open(nameImage, 'wb') as fileImage:
+        fileImage.write(r.content)
+
+
 def main(url):
-	urlimage = GetUrlImage(url)
-	
-	if type(urlimage) is list:
-		for urls in urlimage:
-			DowloadImage(GetNameImage(urls), urls)
-	else:
-		DowloadImage(GetNameImage(urlimage),urlimage)
+    urlimage = GetUrlImage(url)
+
+    if type(urlimage) is list:
+        for urls in urlimage:
+            DowloadImage(GetNameImage(urls), urls)
+    else:
+        DowloadImage(GetNameImage(urlimage), urlimage)
+
 
 if __name__ == '__main__':
-	main(sys.argv[1])
+    main(sys.argv[1])
