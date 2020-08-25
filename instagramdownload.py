67cc24eac1f3c5697074d637dfb7bcbed3c36fa0
lastorder999 <max_obey@hotmail.com>
2020-08-25 12:58:19 +0700

    update name variable

--- instagramdownload.py
+++ instagramdownload.py
@@ -1,58 +1,80 @@
-#!/usr/bin python3
+#!/usr/bin/python3
 import requests
-import sys
 
 
-def URLSingleImage(jsonFile):
-    return jsonFile['graphql']['shortcode_media']['display_url']
+def url_single_image(content):
+    return content['display_url']
 
 
-def URLMultipleMedia(jsonFile):
-    findURL = jsonFile['graphql']['shortcode_media'][
-        'edge_sidecar_to_children']['edges']
-    return [URLs['node']['display_url'] for URLs in findURL]
+def url_single_video(content):
+    return content['video_url']
 
 
-def URLSingleVideo(jsonFile):
-    return jsonFile['graphql']['shortcode_media']['video_url']
+def url_multiple_media(content):
+    all_media = content['edge_sidecar_to_children']['edges']
+    return [categorise_media(each_media['node']) for each_media in all_media]
 
 
-def GetUrlImage(URL):
-    add = '?__a=1'
-    link = URL + add
-    r = requests.get(link)
-    jsonFile = r.json()
-    typeFile = jsonFile['graphql']['shortcode_media']['__typename']
-    if typeFile == "GraphImage":
-        return URLSingleImage(jsonFile)
-    elif typeFile == "GraphVideo":
-        return URLSingleVideo(jsonFile)
-    elif typeFile == "GraphSidecar":
-        return URLMultipleMedia(jsonFile)
+def check_is_video(content):
+    return content['is_video'] is True
 
 
-def GetNameImage(URLImage):
-    URLSplit = URLImage.split('/')[-1]
-    URLSplit = URLSplit.split('?')[0]
-    return URLSplit
+def categorise_media(content):
+    if check_is_video(content):
+        return url_single_video(content)
+    return url_single_image(content)
 
 
-def DowloadImage(nameImage, URLImage):
-    r = requests.get(URLImage)
-
-    with open(nameImage, 'wb') as fileImage:
-        fileImage.write(r.content)
+def go_into_shortcode_media(content):
+    return content['graphql']['shortcode_media']
 
 
-def main(url):
-    urlimage = GetUrlImage(url)
+def go_into_type_name(content):
+    return content['__typename']
 
-    if type(urlimage) is list:
-        for urls in urlimage:
-            DowloadImage(GetNameImage(urls), urls)
-    else:
-        DowloadImage(GetNameImage(urlimage), urlimage)
+
+def concatenate_url(url):
+    rear = '?__a=1'
+    return url + rear
+
+
+def find_url_media(url_instagram):
+    url_instagram_json = concatenate_url(url_instagram)
+    content = requests.get(url_instagram_json)
+    content_json = content.json()
+    content_at_shortcode_media = go_into_shortcode_media(content_json)
+    type_file = go_into_type_name(content_at_shortcode_media)
+
+    if type_file == 'GraphImage':
+        url_media = [url_single_image(content_at_shortcode_media)]
+    elif type_file == 'GraphVideo':
+        url_media = [url_single_video(content_at_shortcode_media)]
+    elif type_file == 'GraphSidecar':
+        url_media = url_multiple_media(content_at_shortcode_media)
+    return url_media
+
+
+def generate_name(url_media):
+    name_media = url_media.split('/')[-1].split('?')[0]
+    return name_media
+
+
+def download_media(url_media):
+    content_media = requests.get(url_media)
+    name_media = generate_name(url_media)
+
+    with open(name_media, 'wb') as file:
+        file.write(content_media.content)
+    print(f'File name: {name_media}')
+
+
+def main():
+    while True:
+        something = input('Enter url instagram >> ').strip()
+        url_media = find_url_media(something)
+        for url in url_media:
+            download_media(url)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
