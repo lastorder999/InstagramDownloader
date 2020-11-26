8788a3f23be6def6231c1f0cabeb0f421a47c5cc
lastorder999 <max_obey@hotmail.com>
2020-11-27 04:52:14 +0700

    add func incrementing filename

--- instagramdownload.py
+++ instagramdownload.py
@@ -1,12 +1,49 @@
-#!/usr/bin/python3
+import os
 import requests
 
 
+def check_file_exists(name):
+    """Check if a file exist."""
+    return os.path.isfile(name)
+
+
+def create_name_pattern(name):
+    """Create a name pattern.
+
+    Arge:
+        name: 'file.jpg'
+
+    Returns:
+        name: 'file-{}.jpg'
+    """
+    file_name, file_type = name.split('.')
+    new_file_name = file_name + '-{}.'
+    return new_file_name + file_type
+
+
+def increment_file_name(name_pattern):
+    """Find file name which does not exist.
+
+    Arge:
+        name_pattern: 'file-{}.jpg'
+
+    Returns:
+        file-1.jpg
+        file-2.jpg
+    """
+    i = 1
+    while os.path.exists(name_pattern.format(i)):
+        i += 1
+    return name_pattern.format(i)
+
+
 def url_single_image(content):
+    """Find image URL."""
     return content['display_url']
 
 
 def url_single_video(content):
+    """Find video URL."""
     return content['video_url']
 
 
@@ -16,10 +53,13 @@ def url_multiple_media(content):
 
 
 def check_is_video(content):
+    """Check content if is video."""
     return content['is_video'] is True
 
 
 def categorise_media(content):
+    """Make sure the content is video or image if it's video call function find
+    video URL, but if it's image call function find image URL."""
     if check_is_video(content):
         return url_single_video(content)
     return url_single_image(content)
@@ -30,6 +70,7 @@ def go_into_shortcode_media(content):
 
 
 def go_into_type_name(content):
+    """Find content type."""
     return content['__typename']
 
 
@@ -39,6 +80,7 @@ def concatenate_url(url):
 
 
 def find_url_media(url_instagram):
+    """Classify"""
     url_instagram_json = concatenate_url(url_instagram)
     content = requests.get(url_instagram_json)
     content_json = content.json()
@@ -55,25 +97,39 @@ def find_url_media(url_instagram):
 
 
 def generate_name(url_media):
+    """Separate the name from URL."""
     name_media = url_media.split('/')[-1].split('?')[0]
     return name_media
 
 
-def download_media(url_media):
+def download_media(name_media, url_media):
+    """Save image or video by write binary."""
     content_media = requests.get(url_media)
-    name_media = generate_name(url_media)
 
     with open(name_media, 'wb') as file:
         file.write(content_media.content)
-    print(f'File name: {name_media}')
+    print('File name: {}'.format(name_media))
 
 
 def main():
     while True:
         something = input('Enter url instagram >> ').strip()
-        url_media = find_url_media(something)
-        for url in url_media:
-            download_media(url)
+        url_medias = find_url_media(something)
+        if not url_medias:
+            continue
+        for url in url_medias:
+            name_media = generate_name(url)
+            if check_file_exists(name_media):
+                print('keep/ replace/ skip')
+                choice = input('>> ')
+                if choice == 'keep':
+                    download_media(increment_file_name(create_name_pattern(name_media)), url)
+                if choice == 'replace':
+                    download_media(name_media, url)
+                else:
+                    continue
+            else:
+                download_media(name_media, url)
 
 
 if __name__ == '__main__':
